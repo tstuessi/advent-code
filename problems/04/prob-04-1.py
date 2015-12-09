6d781e58b2231958312f56fa2b0dc167aaaea786
Tyler Stuessi <computernerdforlife49@gmail.com>
2015-12-09 18:35:31 -0500

    Finished day 4

---
+++ problems/04/prob-04-1.py
@@ -0,0 +1,13 @@
+# I don't actually know anything about 
+# md5 hashing, so this solution came from 
+# the help of cinnamennen
+
+import hashlib
+
+key = raw_input("key: ")
+i = 1
+
+while hashlib.md5(key+str(i)).hexdigest().startswith('00000') is False:
+    i += 1
+
+print i
